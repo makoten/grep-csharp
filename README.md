733364ce404ba34cede4f094a872b8bfa34748ed
makoten <makoten@proton.me>
2024-09-30 21:33:38 +0300

    docs: add method descriptions

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Build Your Own Grep
 
 ## Description
+
 The goal of this hobby project is to imitate a fraction of the power of `grep`.
 I created it strictly for learning purposes and is not meant for production.
 
 ## Usage
+
 1. Ensure you have dotnet (8.0) installed locally
 2. Run ./your_program.sh to run your program, which is implemented in src/Program.cs.
+
+## Supported Expressions
+
+- explicit letters (case sensitive)
+- multiple letters `+`
+- positive/negative groups `[abc]`, `[^abc]`
+- explicit start and end of a sentence `^` and `$`
--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,11 @@
-
-
 if (args[0] != "-E")
 {
     Console.WriteLine("Expected first argument to be '-E'");
     Environment.Exit(2);
 }
 
-// string pattern = "ca+t";
-// string inputLine = "act";
-string pattern = args[1];
-string inputLine = Console.In.ReadToEnd();
+var pattern = args[1];
+var inputLine = Console.In.ReadToEnd();
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -18,12 +14,12 @@ var matcher = new PatternMatcher(pattern);
 Environment.Exit(matcher.MatchWithInput(inputLine) ? 0 : 1);
 
 /// <summary>
-/// This class provides the functionality to pattern match various inputs 
-/// with a specific regular expression that must be provided at object creation.
-/// It doesn't use Regex built-in methods, that would make the class a simple wrapper and bloat.
+///     This class provides the functionality to pattern match various inputs
+///     with a specific regular expression that must be provided at object creation.
+///     It doesn't use Regex built-in methods, that would make the class a simple wrapper and bloat.
 /// </summary>
 /// <param name="pattern">The regular expression that we'll be used to match</param>
-class PatternMatcher(string pattern)
+internal class PatternMatcher(string pattern)
 {
     public bool MatchWithInput(string input)
     {
@@ -37,7 +33,7 @@ class PatternMatcher(string pattern)
             var lookup = pattern[start..end];
             return !input.Any(x => lookup.Contains(x, StringComparison.InvariantCulture));
         }
-    
+
         if (HasPositiveCharacterGroups(pattern))
         {
             var start = pattern.IndexOf('[') + 1;
@@ -45,23 +41,31 @@ class PatternMatcher(string pattern)
             var lookup = pattern[start..end];
             return input.Any(x => lookup.Contains(x, StringComparison.InvariantCulture));
         }
+
         var inputIdx = 0;
         var matchFound = false;
         if (pattern[0] == '^')
             return Matcher(input, inputIdx);
-    
+
         while (inputIdx < input.Length)
         {
             matchFound = Matcher(input, inputIdx);
             if (matchFound)
                 return true;
-    
+
             inputIdx++;
         }
-    
+
         return false;
     }
-    
+
+    /// <summary>
+    ///     This method checks whether a substring of the current input
+    ///     satisfies the encapsulated pattern.
+    /// </summary>
+    /// <param name="input">the provided input</param>
+    /// <param name="i">the index used to read characters from the input</param>
+    /// <returns>True if the input satisfies the pattern, false if the pattern fails at any point.</returns>
     private bool Matcher(string input, int i)
     {
         // edge case for strict string start
@@ -72,22 +76,19 @@ class PatternMatcher(string pattern)
             if (i == input.Length)
                 // handle the end of line pattern match
                 return pattern[patternIdx] == '$';
-    
+
             if (pattern[patternIdx] == '\\')
             {
                 patternIdx++;
-                if (pattern[patternIdx] == 'w' && !(char.IsLetterOrDigit(input[i]) || input[i] == '_') ||
-                    pattern[patternIdx] == 'd' && !char.IsNumber(input[i]))
+                if ((pattern[patternIdx] == 'w' && !(char.IsLetterOrDigit(input[i]) || input[i] == '_')) ||
+                    (pattern[patternIdx] == 'd' && !char.IsNumber(input[i])))
                     return false;
             }
             else if (pattern[patternIdx] == '+')
             {
                 var repeatingLetter = pattern[patternIdx - 1];
-                while (input[i] == repeatingLetter && i < input.Length)
-                {
-                    i++;
-                }
-                
+                while (input[i] == repeatingLetter && i < input.Length) i++;
+
                 // correcting offset input index due to repeated character matching
                 i--;
             }
@@ -95,15 +96,15 @@ class PatternMatcher(string pattern)
             {
                 return false;
             }
-            
-    
+
+
             patternIdx++;
             i++;
         }
-    
+
         return true;
     }
-    
+
     private bool HasPositiveCharacterGroups(string input)
     {
         var openingBracket = false;
@@ -130,14 +131,9 @@ class PatternMatcher(string pattern)
 
     private bool HasNegativeCharacterGroups(string input)
     {
-        if (HasPositiveCharacterGroups(input))
-        {
-            for (var i = 0; i < input.Length - 1; i++)
-            {
-                if (input[i] == '[' && input[i + 1] == '^')
-                    return true;
-            }
-        }
+        for (var i = 0; i < input.Length - 1; i++)
+            if (input[i] == '[' && input[i + 1] == '^')
+                return true;
 
         return false;
     }
